fa1d2613542c87dde566f53ba71798197773144f
Sean Gillespie <sean@swgillespie.me>
2022-12-02 00:02:06 -0800

    Initial commit

---
+++ Makefile
@@ -0,0 +1,26 @@
+CPP := gcc
+CC := scc
+LD := gcc
+
+SOURCES := $(shell find . -name "*.c")
+PREPROC_SOURCES := $(SOURCES:.c=.preproc.c)
+ASM_SOURCES := $(SOURCES:.c=.s)
+PROGRAMS := $(basename $(ASM_SOURCES))
+
+all: $(PROGRAMS)
+
+%: %.c
+
+%.preproc.c: %.c
+	$(CPP) -E -P $< > $@
+
+%.s: %.preproc.c
+	$(CC) $< > $@
+
+%: %.s
+	$(LD) -static -o $@ $<
+
+clean:
+	rm -f $(PROGRAMS)
+	rm -f $(PREPROC_SOURCES)
+	rm -f $(ASM_SOURCES)
---
+++ day1.c
@@ -0,0 +1,41 @@
+int
+gets();
+
+int
+puts();
+
+int
+strlen();
+
+int
+printf();
+
+int
+atoi();
+
+int
+main()
+{
+  char buffer[10];
+  int elves[400];
+  int count = 0;
+  elves[0] = 0;
+  while (gets(buffer) != 0) {
+    if (strlen(buffer) == 0) {
+      count++;
+      elves[count] = 0;
+    } else {
+      elves[count] = elves[count] + atoi(buffer);
+    }
+  }
+
+  for (int i = 1; i < count; i++) {
+    for (int j = i; 0 <= j && elves[j] < elves[j - 1]; j--) {
+      int tmp = elves[j - 1];
+      elves[j - 1] = elves[j];
+      elves[j] = tmp;
+    }
+  }
+
+  printf("%d\n", elves[count - 1] + elves[count - 2] + elves[count - 3]);
+}
